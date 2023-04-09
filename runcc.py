a7255ca804feff4e4a0dc3d42cb8f673d237988e
antogit-sys <antonioperrgiorgi@gmail.com>
2023-04-09 19:07:45 +0200

    create runcc.py

---
+++ runcc.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# --- Import module and method
+import argparse
+from os import path
+from subprocess import run
+
+# -- defined main function
+def main():
+	pass
+
+if __name__ == '__main__':
+	main()
+
+
