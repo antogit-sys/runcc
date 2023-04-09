e039e7af3b139f4689680d9fc871fecdbd4ba3fe
antogit-sys <antonioperrgiorgi@gmail.com>
2023-04-09 19:20:58 +0200

    use argparse

--- runcc.py
+++ runcc.py
@@ -7,8 +7,10 @@ from subprocess import run
 
 # -- defined main function
 def main():
-	pass
-
+	parser = argparse.ArgumentParser(description="Simple execute source code C/C++")
+	parser.add_argument("filename", type=str, help="filename of the source code C/C++")
+	args = parser.parse_args()
+	
 if __name__ == '__main__':
 	main()
 
